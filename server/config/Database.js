f0737de06df75819f1f4da62da36f5853e6526e5
sourabhbadgaiya2 <sourabhbadgaiya2@gmail.com>
2024-11-23 15:02:47 +0530

    complete

--- server/config/Database.js
+++ server/config/Database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
-    console.log(`db is connected`);
+    console.log(`database is connected`);
   })
   .catch((error) => {
     console.log(error.message);
