d198fba70707ee1d1a134633a42030c93b1bb06b
sourabhbadgaiya2 <sourabhbadgaiya2@gmail.com>
2024-11-23 15:42:40 +0530

    fixesss

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use("/bookings", require("./routes/BookingRoutes"));
 
 const path = require("path");
 
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
